5c15eb964f2fe9ab1c0309cfc73818a153553089
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-08 15:08:03 +0200

    Add some functions that interact with user model

---
+++ src/models/user.server.ts
@@ -0,0 +1,27 @@
+import type { Password, User } from '@prisma/client'
+import * as argon from 'argon2'
+
+import { prisma } from '~/lib'
+
+type CreateUserInput = {
+  email: User['email']
+  password: Password['hash']
+}
+
+export const getUserByEmail = async (email: string) =>
+  prisma.user.findUnique({ where: { email } })
+
+export const getUserWithPassword = async (email: string) =>
+  prisma.user.findUnique({
+    where: { email },
+    select: { id: true, email: true, password: true }
+  })
+
+export const createUser = async (data: CreateUserInput) => {
+  const { password, ...dto } = data
+  const hash = await argon.hash(password)
+
+  return prisma.user.create({
+    data: { ...dto, password: { create: { hash } } }
+  })
+}
