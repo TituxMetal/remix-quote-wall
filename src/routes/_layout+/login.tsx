29dba01538e7d8a932ccf56a980bca2ae8ea0e83
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-08 15:22:17 +0200

    Update login page component to use remix-auth and remix-forms

--- src/routes/_layout+/login.tsx
+++ src/routes/_layout+/login.tsx
@@ -1,134 +1,64 @@
 import type {
   ActionArgs,
   ActionFunction,
+  LoaderArgs,
+  LoaderFunction,
   V2_MetaFunction
 } from '@remix-run/node'
-import { Link, useActionData, useSearchParams } from '@remix-run/react'
+import { Link } from '@remix-run/react'
+import { makeDomainFunction } from 'domain-functions'
+import { AuthorizationError } from 'remix-auth'
+import { performMutation } from 'remix-forms'
 
-import { authService, sessionService } from '~/services'
-import {
-  badRequest,
-  validateEmail,
-  validatePassword,
-  validateUrl
-} from '~/utils'
+import { RemixForm } from '~/components/form'
+import { AuthSchema } from '~/schemas'
+import { authenticator } from '~/services'
 
 export const meta: V2_MetaFunction = () => [{ title: 'Login to your account' }]
 
 export const action: ActionFunction = async ({ request }: ActionArgs) => {
-  const formData = await request.formData()
-  const { email, password, redirectTo } = Object.fromEntries(formData)
+  await performMutation({
+    request,
+    schema: AuthSchema,
+    mutation: makeDomainFunction(AuthSchema)(async () => {})
+  })
 
-  if (
-    typeof email !== 'string' ||
-    typeof password !== 'string' ||
-    typeof redirectTo !== 'string'
-  ) {
-    return badRequest({ formError: `Invalid form submission.` })
+  try {
+    return await authenticator.authenticate('login', request, {
+      successRedirect: '/quote/new'
+    })
+  } catch (error) {
+    if (error instanceof AuthorizationError) {
+      return { errors: { password: ['Invalid credentials!'] } }
+    }
+
+    throw error
   }
-
-  const validRedirectTo = validateUrl(redirectTo || '/')
-  const fields = { email, password }
-  const fieldErrors = {
-    email: validateEmail(email),
-    password: validatePassword(password)
-  }
-
-  if (Object.values(fieldErrors).some(Boolean)) {
-    return badRequest({ fieldErrors, fields })
-  }
-
-  const user = await authService.login({ email, password })
-
-  if (!user) {
-    return badRequest({ fields, formError: 'Invalid Credentials' })
-  }
-
-  return sessionService.createUserSession(user.id, validRedirectTo)
 }
 
-const inputClassName = `w-full rounded border-2 border-purple-100 bg-transparent px-2 py-1 text-xl text-purple-950`
+export const loader: LoaderFunction = async ({ request }: LoaderArgs) =>
+  authenticator.isAuthenticated(request, { successRedirect: '/' })
 
 const LoginPage = () => {
-  const actionData = useActionData()
-  const [searchParams] = useSearchParams()
-
   return (
     <div className='flex content-center items-center justify-center'>
       <section className='gradient my-10 rounded-md px-5 py-6 font-bold md:w-2/3 lg:m-10 lg:w-1/2'>
         <h1 className='mb-6 text-center text-4xl font-bold text-purple-100'>
           Login to your account
         </h1>
-        <form method='post' className='flex flex-col gap-4'>
-          <input
-            type='hidden'
-            name='redirectTo'
-            value={searchParams.get('redirectTo') ?? undefined}
-          />
-          <fieldset className='flex justify-center'>
-            <Link to='/register'>Need an Account?</Link>
-          </fieldset>
-          <label className='text-xl leading-7 text-purple-100'>
-            Email:
-            <input
-              className={inputClassName}
-              defaultValue={actionData?.fields?.email}
-              aria-invalid={Boolean(actionData?.fieldErrors?.email)}
-              aria-errormessage={
-                actionData?.fieldErrors?.email ? 'email-error' : undefined
-              }
-              type='email'
-              name='email'
-              required
-            />
-            {actionData?.fieldErrors?.email && (
-              <p
-                className='mt-2 font-bold text-pink-200'
-                role='alert'
-                id='email-error'
-              >
-                {actionData.fieldErrors.email}
-              </p>
-            )}
-          </label>
-          <label className='text-xl leading-7 text-purple-100'>
-            Password:
-            <input
-              className={inputClassName}
-              defaultValue={actionData?.fields?.password}
-              aria-invalid={Boolean(actionData?.fieldErrors?.password)}
-              aria-errormessage={
-                actionData?.fieldErrors?.password ? 'password-error' : undefined
-              }
-              type='password'
-              name='password'
-              minLength={6}
-              required
-            />
-            {actionData?.fieldErrors?.password && (
-              <p
-                className='mt-2 font-bold text-pink-200'
-                role='alert'
-                id='password-error'
-              >
-                {actionData.fieldErrors.password}
-              </p>
-            )}
-          </label>
-          {actionData?.formError && (
-            <div id='form-error-message'>
-              <p className='mt-2 font-bold text-pink-200' role='alert'>
-                {actionData.formError}
-              </p>
-            </div>
+        <p className='flex justify-center'>
+          <Link to='/register'>Need an Account?</Link>
+        </p>
+        <RemixForm schema={AuthSchema} buttonLabel='Login'>
+          {({ Field, Button, Errors }) => (
+            <>
+              <Field name='email' />
+              <Field name='password' type='password' />
+              <Errors />
+              <Button />
+            </>
           )}
-          <button
-            className='w-min rounded-lg border-2 border-purple-100 bg-purple-700 px-10 py-3 font-bold hover:scale-105'
-            type='submit'
-          >
-            Login
-          </button>
-        </form>
+        </RemixForm>
       </section>
     </div>
   )
