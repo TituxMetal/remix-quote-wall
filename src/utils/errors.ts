0f49854ccde45074213ab41abcad3c59945a8727
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-08 15:00:02 +0200

    Add getErrorMessage function in utilities

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,3 +1,17 @@
 import { json } from '@remix-run/node'
 
 export const badRequest = (data: any) => json(data, { status: 400 })
+
+type Props = {
+  min?: number
+  max?: number
+}
+export const getErrorMessage = (props?: Props) => {
+  const { min, max } = props || {}
+
+  return {
+    isEmail: `Please enter a valid email.`,
+    minLength: `This field must contain at least ${min} character(s).`,
+    maxLength: `This field must contain at most ${max} character(s).`
+  }
+}
