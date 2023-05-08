b2397b61185c9fdb8d87c54c9fa5678b22564ac5
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-08 15:16:26 +0200

    Add auth schema

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+import { getErrorMessage } from '~/utils'
+
+export const AuthSchema = z.object({
+  email: z.string().email({ message: getErrorMessage().isEmail }).trim(),
+  password: z
+    .string()
+    .trim()
+    .min(6, getErrorMessage({ min: 6 }).minLength)
+})
