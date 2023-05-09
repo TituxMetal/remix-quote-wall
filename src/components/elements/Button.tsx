790518af395c229fbb84eef99a81674beff5ee32
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-09 06:53:32 +0200

    Change button component class name

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@slicknode/stylemapper'
 
-const buttonClasses = `w-min rounded-lg border-2 border-purple-100 bg-purple-700 px-10 py-3 font-bold hover:scale-105`
+const buttonClasses = `w-max rounded-lg border-2 border-purple-100 bg-purple-700 px-4 py-3 font-bold hover:scale-105`
 
 const AppButton = styled('button', buttonClasses)
 
