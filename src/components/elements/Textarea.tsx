af3c6066d4a6affa018722ef7f7585fe6a6531c4
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-09 06:51:44 +0200

    Add textarea component

---
+++ src/components/elements/Textarea.tsx
@@ -0,0 +1,17 @@
+import { styled } from '@slicknode/stylemapper'
+import type { TextareaHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+
+type Ref = HTMLTextAreaElement
+type Props = TextareaHTMLAttributes<Ref>
+
+const AppTextarea = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <textarea ref={ref} className={className} {...props} />
+))
+AppTextarea.displayName = 'Textarea'
+
+const textareaClassName = `h-44 min-h-full w-full resize-none rounded border-2 border-purple-100 bg-transparent px-2 py-1 text-xl text-purple-950`
+
+const StyledTextarea = styled(AppTextarea, textareaClassName)
+
+export default StyledTextarea
--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,3 +1,4 @@
 export * from './Button'
 export * from './Error'
 export { default as Input } from './Input'
+export { default as Textarea } from './Textarea'
