1f4a83c44b436a09f9db0688e6d646e286aff465
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-09 06:59:34 +0200

    Update new quote page component, use remix-forms

--- src/routes/_layout+/quote.new.tsx
+++ src/routes/_layout+/quote.new.tsx
@@ -5,10 +5,12 @@ import type {
   LoaderFunction,
   V2_MetaFunction
 } from '@remix-run/node'
-import { redirect } from '@remix-run/node'
-import { json } from '@remix-run/node'
+import { makeDomainFunction } from 'domain-functions'
 
-import { prisma } from '~/lib'
+import { RemixForm } from '~/components/form'
+import { formAction } from '~/lib/form-action.server'
+import { createQuote } from '~/models'
+import { QuoteSchema } from '~/schemas'
 import { authenticator } from '~/services'
 
 export const meta: V2_MetaFunction = () => [
@@ -19,31 +21,21 @@ export const action: ActionFunction = async ({ request }: ActionArgs) => {
   const { id } = await authenticator.isAuthenticated(request, {
     failureRedirect: '/login'
   })
-  const formData = await request.formData()
-  const { text, by } = Object.fromEntries(formData)
 
-  if (
-    typeof text !== 'string' ||
-    text.length === 0 ||
-    typeof by !== 'string' ||
-    by.length === 0
-  ) {
-    return json({ error: 'Form not submitted correctly.' }, { status: 400 })
-  }
-
-  const fields = { text, by }
-
-  await prisma.quote.create({ data: { userId: id, ...fields } })
-
-  return redirect('/')
+  return formAction({
+    request,
+    schema: QuoteSchema,
+    successPath: '/',
+    mutation: makeDomainFunction(QuoteSchema)(async values =>
+      createQuote({ ...values, userId: id })
+    )
+  })
 }
 
 export const loader: LoaderFunction = async ({ request }: LoaderArgs) =>
   authenticator.isAuthenticated(request, {
     failureRedirect: `/login`
   })
-
-const inputClassName = `w-full rounded border-2 border-purple-100 bg-transparent px-2 py-1 text-xl text-purple-950`
 
 const NewQuotePage = () => {
   return (
@@ -52,27 +44,11 @@ const NewQuotePage = () => {
         <h1 className='mb-6 text-center text-4xl font-bold text-purple-100'>
           Add a new Quote
         </h1>
-        <form method='post' className='flex flex-col gap-4'>
-          <label className='text-xl leading-7 text-purple-100'>
-            Quote Master:
-            <input className={inputClassName} type='text' name='by' required />
-          </label>
-          <label className='text-xl leading-7 text-purple-100'>
-            Quote Text
-            <textarea
-              className={`${inputClassName} h-44 min-h-full resize-none`}
-              name='text'
-              id=''
-              required
-            />
-          </label>
-          <button
-            className='w-min rounded-lg border-2 border-purple-100 bg-purple-700 px-10 py-3 font-bold hover:scale-105'
-            type='submit'
-          >
-            Add
-          </button>
-        </form>
+        <RemixForm
+          schema={QuoteSchema}
+          buttonLabel='Add Quote'
+          multiline={['text']}
+        />
       </section>
     </div>
   )
